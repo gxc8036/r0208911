e5b550db0c9496b8c6c1a9282b59028da6b107e8
guxichang <19951799881@163.com>
2023-12-04 12:37:51 +0800

    up

--- src/practice/r1_base.rs
+++ src/practice/r1_base.rs
@@ -4,40 +4,40 @@ use rand::Rng;
 
 #[test]
 fn guess_num_game() {
-    println!("猜数字游戏!");
+    println!("guess the mum game.");
 
     let secret_num = rand::thread_rng().gen_range(1..101);
 
     loop {
-        println!("请输入你猜的数字 (输入quit将直接终止程序): ");
+        println!("please enter the number (entering quit will terminate the program ): ");
         let mut guess = String::new();
 
         io::stdin()
             .read_line(&mut guess)
-            .expect("不能正确读出输入信息！");
+            .expect("failed to read line.");
 
         let guess = guess.trim();
 
         if guess == "quit" {
-            println!("程序终止!");
+            println!("the program terminated.");
             break;
         }
 
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(err) => {
-                println!("解析失败: {}", err);
+                println!("parse err: {}", err);
                 continue;
             }
         };
 
-        println!("你当前输入的数字是: {guess} ");
+        println!("you guess num is : {guess} ");
 
         match guess.cmp(&secret_num) {
-            Ordering::Less => println!("太小了！"),
-            Ordering::Greater => println!("太大了！"),
+            Ordering::Less => println!("to small!"),
+            Ordering::Greater => println!("too big!"),
             Ordering::Equal => {
-                println!("胜利!");
+                println!("you win!");
                 break;
             }
         }
